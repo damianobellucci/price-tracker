0227d8fa794e94b9edb9cbd41a3990ad620a35e4
Damiano <belluccidamiano@gmail.com>
2021-04-04 18:02:06 +0200

    added server for price scraping

--- service-scrape-price/src.js
+++ service-scrape-price/src.js
@@ -1,13 +1,29 @@
 const phantom = require('phantom');
+const express = require('express');
 
-(async function () {
+const app = express()
+const PORT = 4000
+
+app.get('/', async (req, res) => {
+    url = req.query.url
+    let start = Date.now()
+    const price = await scrapePrice(url);
+    res.send("Price: " + price + " | scraped in " + (Date.now() - start) / 1000 + " seconds")
+})
+
+app.listen(PORT, console.log("listenting on port: " + PORT));
+
+
+//-----------------------------------------------------------------
+const urlExample = "https://www.amazon.it/gp/product/B07NTV64J2/ref=ppx_yo_dt_b_asin_image_o00_s00?ie=UTF8&psc=1"
+
+async function scrapePrice(url) {
     ////phantom part/////
     const instance = await phantom.create();
     const page = await instance.createPage();
     await page.on("onResourceRequested", function (requestData) {
         console.info('Requesting', requestData.url)
     });
-    let url = "https://www.amazon.it/gp/product/B07NTV64J2/ref=ppx_yo_dt_b_asin_image_o00_s00?ie=UTF8&psc=1"
     const status = await page.open(url);
     const content = await page.property('content');
     await instance.exit();
@@ -17,8 +33,8 @@ const phantom = require('phantom');
     target = '<span id="price_inside_buybox" class="a-size-medium a-color-price">'
     indexTarget = data.indexOf(target)
     price = (data.substring(indexTarget + target.length)).substring(0, data.substring(indexTarget + target.length).indexOf('&')).replace(/\s/g, "") //arriving to the first number of the price
-    console.log(price)
-}());
+    return price
+};
 
 
 
